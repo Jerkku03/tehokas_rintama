66c276f0eb3055e28a8fc7f55fb4772bf4c6d8b2
Jere <johiet@utu.fi>
2025-09-25 14:52:12 +0300

    poista

--- main/java/fi/utu/tech/exercise1/calculator/EmptyArrayException.java
+++
@@ -1,4 +0,0 @@
-package fi.utu.tech.exercise1.calculator;
-
-public class EmptyArrayException extends Exception {
-}
--- main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-module fi.utu.tech.exercise {
-    requires kotlin.stdlib;
-    exports fi.utu.tech.exercise1;
-    exports fi.utu.tech.exercise1.calculator;
-}
